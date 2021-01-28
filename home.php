0a6f0dc35b95b884e5aaf7fcef93372fabfd1794
dylantrauschke <78026953+dylantrauschke@users.noreply.github.com>
2021-01-28 14:18:55 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,47 @@
-<?php
-    echo 'test'
-    echo 'test2'
-?>
+<!doctype html>
+
+         <html lang="nl">
+
+         <head>
+        <title>apotheek DHR.J.SCHUT</title>
+        <meta name="description" content= "beschrijving inhoud">
+        <meta charset="iso-8859-1">
+        <meta name= "vieuwport" content="width=device-width, initial-scale1.0"
+        
+        <style>
+        body {font-family: Verdana, sans-serif;}
+         <header> h1 {font-size:1em: margin:0: padding:30px 0; color :#8B4513;}
+       </header> img {float:left; border :0px; padding-right: 5px;}
+
+       nav ul {margin:0; padding:o;}
+       nav ul li {display:inline; border-right:1px solit black; margin:0; padding:5px;}
+       nav ul li a {color:#8B4513;}
+        <article>
+        </article>
+        
+        
+        
+        
+        
+        
+         </head>
+   
+         <header> 
+         </header>
+   </style>
+      <body>
+   <div id="pagina"> 
+    
+    <nav>
+    </nav>
+    <article> 
+    <h1> </h1>
+    <p>
+    </p>
+    
+    
+    </article>
+ 
+    </div>   
+       </body>
+</html>
