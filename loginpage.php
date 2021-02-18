378705d8f4285a6bd84042f92e22ca7e401de92a
dylantrauschke <78026953+dylantrauschke@users.noreply.github.com>
2021-02-18 11:36:20 +0100

    duidelijkere login
    duidelijker gemaakt samen met andrea

--- loginpage.php
+++ loginpage.php
@@ -1,10 +1,72 @@
-<?php
-include 'includes/head.inc.php';
-include 'includes/db_connection.php';
+<?php 
+
+function OpenCon()
+    {
+        //database info
+        $dbhost = "localhost";
+        $dbuser = "admin";
+        $dbpass = "admin";
+        $db = "pharmacy";
+
+        //database connection
+        $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
+
+        return  $conn;
+    }
+
+function CloseCon($conn)
+    {
+        //function that closes db connection
+        $conn -> close();
+    }
+
 ?>
+<!doctype HTML>
+<html lang ="nl">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
 
-<div class="login col-md-4 col-md-offset-4"> 
+    <title>Apotheek Dhr. J. Schut</title>
+
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row">
+    <div class="offset-sm-1 col-sm-11">
+    <a href="home.php"><img src="images/logo.png" alt="logo" class="logo"></a>
+    </div>
+    </div>
+<nav>
+<ul class="nav justify-content-center nav-fill">
+  <li class="nav-item">
+    <a class="nav-link active" href="home.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="service.php">Service</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="overzicht.php">Voorlichting</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="contact.php">Contact</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="loginpage.php">Login</a>
+  </li>
+</ul>
+</nav>
+</div>
+
+        
+<div class="loginpage"> 
 <form method="post" action="check.php">
             Gebruikersnaam: <input type="text" name="username" size="20" maxlength="20"><br />
             Wachtwoord: <input type="password" name="wachtwoord" size="20" maxlength="20"><br /><br />
