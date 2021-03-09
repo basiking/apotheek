9ad9e6d8033bc5920a2f3e324e323ec234ff195d
basiking <33873299+basiking@users.noreply.github.com>
2021-03-09 12:46:41 +0100

    made contact form working and centered it

--- bedankt.php
+++ bedankt.php
@@ -1,6 +1,33 @@
 <?php
 include 'includes/head.inc.php';
-include 'includes/db_connection.php';
+
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$streetName = $_POST['streetName'];
+$streetNumber = $_POST['streetNumber'];
+$cityName = $_POST['cityName'];
+$postalCode = $_POST['postalCode'];
+$phoneNumber = $_POST['phoneNumber'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+
+$conn = new mysqli('localhost', 'admin', 'admin', 'pharmacy');
+if($conn->connect_error){
+    die('Connection failed : '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into contact(firstName, lastName, streetName, streetNumber, cityName, postalCode, phoneNumber, email, message)
+    values(?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("sssssssss", $firstName, $lastName, $streetName, $streetNumber, $cityName, $postalCode, $phoneNumber, $email, $message);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+}
+
+
+
 ?>
 
 <p>Bedankt voor het invullen van het formulier</p>
--- contact.php
+++ contact.php
@@ -3,47 +3,52 @@ include 'includes/head.inc.php';
 include 'includes/db_connection.php';
 ?>
 
-<div class='container-fluid col-md-4 col-md-offset-4'>
+<div class='container-fluid col-md-4 col-md-offset-4 center_div'>
     <h3>Contactpagina</h3>
     <form action='bedankt.php' method='post'>
         <div class="form-group">
             <label for="firstname">Voornaam:</label>
-            <input type="text" class="form-control" id="firstName" 
+            <input type="text" class="form-control" id="firstName" name="firstName"
                 placeholder="Vul hier uw voornaam in">
         </div>
         <div class="form-group">
             <label for="lastname">Achternaam:</label>
-            <input type="text" class="form-control" id="lastName" 
+            <input type="text" class="form-control" id="lastName" name="lastName"
                 placeholder="Vul hier uw achternaam in">
         </div>
         <div class="form-group">
             <label for="streetName">Straat:</label>
-            <input type="text" class="form-control" id="streetName" 
+            <input type="text" class="form-control" id="streetName" name="streetName"
                 placeholder="Vul hier uw straatnaam in">
         </div>
         <div class="form-group">
             <label for="streetNumber">Huisnummer:</label>
-            <input type="text" class="form-control" id="streetNumber" 
+            <input type="text" class="form-control" id="streetNumber" name="streetNumber"
                 placeholder="Vul hier uw huisnummer in">
         </div>
         <div class="form-group">
             <label for="cityName">Stad:</label>
-            <input type="text" class="form-control" id="cityName" 
+            <input type="text" class="form-control" id="cityName" name="cityName"
                 placeholder="Vul hier uw stad in">
         </div>
         <div class="form-group">
             <label for="postalCode">Postcode:</label>
-            <input type="text" class="form-control" id="postalCode" 
+            <input type="text" class="form-control" id="postalCode" name="postalCode"
                 placeholder="Vul hier uw postcode in">
         </div>
         <div class="form-group">
+            <label for="phoneNumber">Telefoonnummber:</label>
+            <input type="text" class="form-control" id="phoneNumber" name="phoneNumber"
+                placeholder="Vul hier uw telefoonnummer in">
+        </div>
+        <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" 
+            <input type="email" class="form-control" id="email" name="email"
                 placeholder="Vul hier uw e-mailadres in">
         </div>
         <div class="form-group">
             <label for="message">Bericht:</label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
+            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Versturen</button>
     </form>
